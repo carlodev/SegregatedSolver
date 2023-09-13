d31aa62ebe5622c316810abb54fdc0f8c14444a9
carlodev <carlobrunellibs@gmail.com>
2023-09-13 17:36:54 +0200

    Naming LinearSolvers, provide options as string

--- run_SegregatedSolver.jl
+++ run_SegregatedSolver.jl
@@ -1,26 +1,25 @@
 using SegregatedSolver
 using Parameters,PartitionedArrays
 
-petsc_options = "-vel_ksp_type gmres -vel_pc_type gamg -vel_ksp_rtol 1.e-12  \
-                  -pres_ksp_type gmres -pres_pc_type gamg -pres_ksp_atol 1.e-5 \
-                  -ksp_converged_reason -log_view"
+ petsc_options = " -vel_ksp_type gmres -vel_pc_type gamg    -vel_ksp_rtol 1.e-10 -vel_ksp_atol 0.0 -vel_ksp_converged_reason \
+                   -pres_ksp_type preonly -pres_pc_type lu  -pres_ksp_atol 1.e-10 -pres_ksp_converged_reason"
 
 params = Dict(
       :N => 50,
       :D => 2, #Dimension
       :order => 1, 
       :t0 => 0.0,
-      :dt => 0.005,
+      :dt => 0.01,
       :tF => 2.5,
-      :case => "Cylinder",
+      :case => "TaylorGreen",
       :θ => 0.5,
       :u_in=> 10.0,
-      :M=> 2, #internal iterations
+      :M=> 5, #internal iterations
       :backend => with_mpi,  #or with_mpi() with_debug()
       :rank_partition=>(2, 2),
       :ν => 0.001,
-      :petsc_options => "-ksp_converged_reason",
-      :method=>:SUPG,
+      :petsc_options => petsc_options,
+      :method=>:VMS,
       :Cᵢ => [4, 36],
       :benchmark=>false,
       :t_endramp=> 0.0,
